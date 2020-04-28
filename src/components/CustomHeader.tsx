9dd1a83c95cf51214d48dc15382456b60f65f584
Nouman Sakhawat <48842733+NoumanSakhawat@users.noreply.github.com>
2020-04-28 14:51:12 +0500

    Update CustomHeader.tsx

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
-import { View, TouchableOpacity, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, StatusBar, SafeAreaView, StyleProp, ViewStyle } from 'react-native';
 import colors from "../constants/colors";
 import CustomIcon from "./CustomIcon";
 import Text from "./CustomText";
 import { marginSizesWidth, marginSizesHeight, fontSizes } from "../constants/sizes";
 
 interface CustomHeaderInterface {
-    style: object;
+    style: StyleProp<ViewStyle>;
     statusBarStyle: "dark-content" | "light-content" | "default";
     statusBarBackgroundColor: string;
     headerBackgroundColor: string;
-    containerStyle: object;
+    containerStyle: StyleProp<ViewStyle>;
 
     leftIconType: 'Ionicons' | 'AntDesign' | 'Entypo' | 'EvilIcons' | 'Feather' | 'FontAwesome' | 'Fontisto' | 'MaterialCommunityIcons' | 'MaterialIcons';
-    leftIconStyle: object;
+    leftIconStyle: StyleProp<ViewStyle>;
     leftIconName: any;
     leftIconSize: number;
     leftIconColor: any;
 
     rightIconType: 'Ionicons' | 'AntDesign' | 'Entypo' | 'EvilIcons' | 'Feather' | 'FontAwesome' | 'Fontisto' | 'MaterialCommunityIcons' | 'MaterialIcons';
-    rightIconStyle: object;
+    rightIconStyle: StyleProp<ViewStyle>;
     rightIconName: any;
     rightIconSize: number;
     rightIconColor: any;
 
-    leftIconContainerStyle: object;
-    rightIconContainerStyle: object;
-    titleContainerStyle: object;
-    titleStyle: object;
+    leftIconContainerStyle: StyleProp<ViewStyle>;
+    rightIconContainerStyle: StyleProp<ViewStyle>;
+    titleContainerStyle: StyleProp<ViewStyle>;
+    titleStyle: StyleProp<ViewStyle>;
     title: any;
     navigation: any;
     leftIconVisible: boolean;
@@ -43,27 +43,27 @@ const hori = marginSizesWidth._20;
 export default class CustomHeader extends Component<CustomHeaderInterface, any> {
 
     public static defaultProps = {
-        style: null,
+        style: {},
         statusBarStyle: "light-content",
         statusBarBackgroundColor: colors.statusbar,
         headerBackgroundColor: colors.header,
-        containerStyle: null,
+        containerStyle: {},
 
         leftIconType: 'Ionicons',
         leftIconName: 'md-arrow-back',
         leftIconColor: colors.white,
         leftIconSize: 30,
-        leftIconStyle: null,
+        leftIconStyle: {},
 
         rightIconType: 'Ionicons',
         rightIconName: 'md-search',
         rightIconColor: colors.white,
         rightIconSize: 30,
-        rightIconStyle: null,
+        rightIconStyle: {},
 
-        leftIconContainerStyle: null,
-        rightIconContainerStyle: null,
-        titleContainerStyle: null,
+        leftIconContainerStyle: {},
+        rightIconContainerStyle: {},
+        titleContainerStyle: {},
         title: "Default Title",
         navigation: null,
         leftIconVisible: true,
@@ -91,25 +91,25 @@ export default class CustomHeader extends Component<CustomHeaderInterface, any> 
             title = `${title.substring(0, 20)}...`
         }
         return (
-            <View style={{ ...styles.container, ...containerStyle }}>
+            <View style={[containerStyle, { ...styles.container }]}>
                 <SafeAreaView style={{ ...styles.safeAreaView, backgroundColor: headerBackgroundColor }} />
 
                 <StatusBar barStyle={statusBarStyle} backgroundColor={statusBarBackgroundColor} />
-                <View style={{ ...styles.headerContainer, ...style, backgroundColor: headerBackgroundColor }}>
+                <View style={[style, { ...styles.headerContainer, backgroundColor: headerBackgroundColor }]}>
 
                     {leftIconVisible ?
-                        <TouchableOpacity style={{ ...styles.leftIconContainer, ...leftIconContainerStyle }} onPress={() => {
+                        <TouchableOpacity style={[leftIconContainerStyle, { ...styles.leftIconContainer }]} onPress={() => {
                             leftIconPress()
                         }}>
                             <CustomIcon name={leftIconName} color={leftIconColor} size={leftIconSize} style={leftIconStyle} iconType={leftIconType} />
                         </TouchableOpacity>
                         : <View />}
 
-                    <View style={{ ...styles.titleContainer, ...titleContainerStyle, marginLeft: leftIconVisible ? -hori : 0 }}>
-                        <Text style={{ ...styles.titleStyle, ...titleStyle }}>{title}</Text>
+                    <View style={[titleContainerStyle, { ...styles.titleContainer, marginLeft: leftIconVisible ? -hori : 0 }]}>
+                        <Text style={[titleStyle, { ...styles.titleStyle, }]}>{title}</Text>
                     </View>
                     {rightIconVisible ?
-                        <TouchableOpacity style={{ ...styles.rightIconContainerStyle, ...rightIconContainerStyle }} onPress={() => {
+                        <TouchableOpacity style={[rightIconContainerStyle, { ...styles.rightIconContainerStyle, }]} onPress={() => {
                             rightIconPress()
                         }}>
                             <CustomIcon name={rightIconName} color={rightIconColor} size={rightIconSize} style={rightIconStyle} iconType={rightIconType} />
