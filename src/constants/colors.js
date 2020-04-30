d2d9fdbcbb0f5c025a5e6e3b459df32cb5f8b701
Nouman Sakhawat <48842733+NoumanSakhawat@users.noreply.github.com>
2020-04-30 11:50:38 +0500

    Update colors.js

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,13 +1,5 @@
 export default {
-    statusbar: "#313d4b",
-    background: "#f7f7f7",
+    statusbar: "#eeeeee",
+    background: "#eeeeee",
     header: "#313d4b",
-    white: "#fff",
-    transparent: "transparent",
-    heading: "#000000",
-    divider: "#E5E5E5",
-    actionButton: "#fc5356",
-    actionButton0: "rgba(231,76,60,1)",
-    actionButton1: '#9b59b6',
-    actionButton2: 'rgba(252, 83, 86, 1)',
 }
