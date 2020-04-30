c5115f0b1288dc28c0bcb3e1280c04d28e30a638
Nouman Sakhawat <48842733+NoumanSakhawat@users.noreply.github.com>
2020-04-30 11:51:00 +0500

    Update CustomHeader.tsx

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import { FlatList, Modal, SafeAreaView, StatusBar, StyleProp, StyleSheet, TextStyle, TouchableOpacity, View, ViewStyle, TextInput } from 'react-native';
-import colors from "../constants/colors";
 import { fontSizes, marginSizesHeight, marginSizesWidth } from "../constants/sizes";
 import CustomIcon from "./CustomIcon";
 import Text from "./CustomText";
 import { emptyValidate } from "../helper/genericFunctions";
+
+const colors = {
+    statusbar: "#313d4b",
+    background: "#f7f7f7",
+    header: "#313d4b",
+    white: "#fff",
+    transparent: "transparent",
+    heading: "#000000",
+    divider: "#E5E5E5",
+    actionButton: "#fc5356",
+    actionButton0: "rgba(231,76,60,1)",
+    actionButton1: '#9b59b6',
+    actionButton2: 'rgba(252, 83, 86, 1)',
+}
 
 interface CustomHeaderInterface {
     style: StyleProp<ViewStyle>;
