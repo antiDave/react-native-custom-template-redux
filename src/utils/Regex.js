8ca9336b34a09df3e2cfb7748f7d050b70fd397a
Nouman Sakhawat <rnouman28@gmail.com>
2020-05-29 17:23:09 +0500

    Regex Update

--- src/utils/Regex.js
+++ src/utils/Regex.js
@@ -1,4 +1,4 @@
-const Regex = {
+export const Regex = {
     emailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))(\s*)$/,
     verficationCodw: /^\s*[a-z|A-Z|0-9|\s]{6}\s*$/,
     password: /^(?=.*\d).{6,100}$/,
@@ -7,14 +7,14 @@ const Regex = {
 }
 
 export class RegexValidator {
-    static email = text => {
+    email = text => {
         return new Promise(async (resolve) => {
             const res = validator(Regex.emailRegex, text);
             resolve(res);
         })
     };
 
-    static password = text => {
+    password = text => {
         return new Promise(async (resolve) => {
             const res = validator(Regex.password, text);
             resolve(res);
