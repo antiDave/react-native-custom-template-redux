6db400e9f6917519e2f4b7473f21ef842ec749e9
Nouman Sakhawat <rnouman28@gmail.com>
2020-07-09 12:58:08 +0500

    INDEX Updaate

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 /**
  * @format
  */
-require('react-native').unstable_enableLogBox();
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
